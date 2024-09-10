4960a0ac7bf02fd0417fb264a97fa9041ab52b6f
ZENATOZENA1 <kenzenamahendraputra@gmail.com>
2024-09-10 13:22:28 +0700

    pak yopi

---
+++ overlay.js
@@ -0,0 +1,47 @@
+function overlay() {
+    // O S K A M A  1 2
+    // OSIS TARUNA TARUNI ANGKATAN 12
+    document.getElementById("header00").innerHTML = 
+    '<div>'+
+        '<h1>KTKJ</h1>'+
+        '<h4>Kenzena Teknik Komputer Jaringan</h4>'+
+   ' </div>';
+
+    // BERANDA 
+    document.getElementById("header").innerHTML =
+    '<div class="a01">'+
+        '<div class="a02">'+
+            '<div class="l01" id="beranda">'+
+                '<a href="https://kzmpgh.github.io/oskama12"><h4>BERANDA</h4></a>'+
+            '</div>'+
+            '<div class="l01" id="acara">'+
+                '<a href="https://kzmpgh.github.io/oskama12/event/"><h4>ACARA</h4></a>'+
+            '</div>'+
+            '<div class="l01" id="galeri">'+
+                '<a href="https://kzmpgh.github.io/oskama12/galeri/"><h4>GALERI</h4></a>'+
+            '</div>'+  
+            '<div class="a03"></div>'+
+        '</div>'+
+    '</div>';
+
+    // Footer
+    document.getElementById("footer").innerHTML =
+    '<div class="f01">'+
+        '<div class="f02">'+
+            '<h4>Kontak</h4>'+
+            '<a href="https://api.whatsapp.com/send/?phone=6289696643899&text=Selamat%20pagi%2C+sebelumnya+mohon+maaf+menggangu+waktunya%20saya%20ingin..."><h5>Whatsapp</h5></a>'+
+            '<a href="https://www.instagram.com/kenzena_/"><h5>Instragram</h5></a>'+
+        '</div>'+
+        '<div class="f03"></div>'+
+        '<div class="bf01">'+
+            '<h5>Website</h5>'+
+            '<h3>KZMP TKJ </h3>'+
+            '<h5>dari</h5>'+
+            '<a href="https://www.instagram.com/kenzena_/"><h5>KZMP</h5></a>'+
+        '</div>'+
+    '</div>';
+
+    document.getElementById("maintance").innerHTML =
+    '<h2>Halaman Sedang Dalam Perbaikan!!...</h2>';
+
+}
