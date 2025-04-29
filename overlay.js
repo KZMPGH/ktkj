95dc87d3b1db26db9e8e6b55ee99ae04350f2c61
ZENATOZENA1 <kenzenamahendraputra@gmail.com>
2025-04-30 00:46:09 +0700

    update home

--- overlay.js
+++ overlay.js
@@ -14,30 +14,31 @@ document.addEventListener('DOMContentLoaded', function overlay() {
             '<div class="l01" id="beranda">'+
                 '<a href="https://kzmpgh.github.io/ktkj/"><h4>BERANDA</h4></a>'+
             '</div>'+
-            // '<div class="l01" id="acara">'+
-            //     '<a href="https://kzmpgh.github.io/oskama12/event/"><h4>ACARA</h4></a>'+
-            // '</div>'+
-            // '<div class="l01" id="galeri">'+
-            //     '<a href="https://kzmpgh.github.io/oskama12/galeri/"><h4>GALERI</h4></a>'+
-            // '</div>'+  
-            // '<div class="a03"></div>'+
         '</div>'+
     '</div>';
 
     // Footer
     document.getElementById("footer").innerHTML =
-    '<div class="f01">'+
-        '<div class="f02">'+
-            '<h4>Kontak</h4>'+
-            '<a href="https://api.whatsapp.com/send/?phone=6289696643899&text=Selamat%20pagi%2C+sebelumnya+mohon+maaf+menggangu+waktunya%20saya%20ingin..."><h5>Whatsapp</h5></a>'+
-            '<a href="https://www.instagram.com/kenzena_/"><h5>Instragram</h5></a>'+
+    '<div class="footer-container">'+
+        '<div class="footer-brand">'+
+            '<div>'+
+                '<h3>KTKJ</h3>'+
+                '<p>Kenzena Teknik Komputer Jaringan</p>'+
+            '</div>'+
         '</div>'+
-        '<div class="f03"></div>'+
-        '<div class="bf01">'+
-            '<h5>Website</h5>'+
-            '<h3>KTKJ </h3>'+
-            '<h5>dari</h5>'+
-            '<a href="https://www.instagram.com/kenzena_/"><h5>KZMP</h5></a>'+
+        '<div class="footer-links">'+
+            '<h4>Kontak</h4>'+
+            '<a href="mailto:ktkj@example.com"><span class="footer-icon">&#9993;</span> Email</a>'+
+            '<a href="https://api.whatsapp.com/send/?phone=6289696643899&text=Halo%20KTKJ"><span class="footer-icon">&#128241;</span> Whatsapp</a>'+
+        '</div>'+
+        '<div class="footer-social">'+
+            '<h4>Ikuti Kami</h4>'+
+            '<a href="https://www.instagram.com/kenzena_/" target="_blank" aria-label="Instagram">'+
+                '<svg width="24" height="24" fill="#FDC11F" viewBox="0 0 24 24"><path d="M12 2.2c3.2 0 3.584.012 4.85.07 1.17.056 1.97.24 2.43.41.59.22 1.01.48 1.45.92.44.44.7.86.92 1.45.17.46.354 1.26.41 2.43.058 1.266.07 1.65.07 4.85s-.012 3.584-.07 4.85c-.056 1.17-.24 1.97-.41 2.43-.22.59-.48 1.01-.92 1.45-.44.44-.86.7-1.45.92-.46.17-1.26.354-2.43.41-1.266.058-1.65.07-4.85.07s-3.584-.012-4.85-.07c-1.17-.056-1.97-.24-2.43-.41-.59-.22-1.01-.48-1.45-.92-.44-.44-.7-.86-.92-1.45-.17-.46-.354-1.26-.41-2.43C2.212 15.784 2.2 15.4 2.2 12s.012-3.584.07-4.85c.056-1.17.24-1.97.41-2.43.22-.59.48-1.01.92-1.45.44-.44.86-.7 1.45-.92.46-.17 1.26-.354 2.43-.41C8.416 2.212 8.8 2.2 12 2.2zm0-2.2C8.736 0 8.332.012 7.052.07 5.77.128 4.89.312 4.13.57c-.8.27-1.48.63-2.16 1.31C1.29 2.56.93 3.24.66 4.04c-.26.76-.44 1.64-.5 2.92C.012 8.332 0 8.736 0 12c0 3.264.012 3.668.07 4.948.058 1.28.24 2.16.5 2.92.27.8.63 1.48 1.31 2.16.68.68 1.36 1.04 2.16 1.31.76.26 1.64.44 2.92.5C8.332 23.988 8.736 24 12 24s3.668-.012 4.948-.07c1.28-.058 2.16-.24 2.92-.5.8-.27 1.48-.63 2.16-1.31.68-.68 1.04-1.36 1.31-2.16.26-.76.44-1.64.5-2.92.058-1.28.07-1.684.07-4.948s-.012-3.668-.07-4.948c-.058-1.28-.24-2.16-.5-2.92-.27-.8-.63-1.48-1.31-2.16-.68-.68-1.36-1.04-2.16-1.31-.76-.26-1.64-.44-2.92-.5C15.668.012 15.264 0 12 0zm0 5.838A6.162 6.162 0 1 0 18.162 12 6.162 6.162 0 0 0 12 5.838zm0 10.162A4 4 0 1 1 16 12a4 4 0 0 1-4 4zm6.406-11.845a1.44 1.44 0 1 0 1.44 1.44 1.44 1.44 0 0 0-1.44-1.44z"/></svg>'+
+            '</a>'+
+        '</div>'+
+        '<div class="footer-copyright">'+
+        '<span>&copy; 2024 KTKJ. All rights reserved.</span>'+
         '</div>'+
     '</div>';
 
